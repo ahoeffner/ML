0a1eac9f058c49d8a048a00180f16165923bdc20
ahoeffner <alex@hoeffner.net>
2025-03-18 13:02:40 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,16 +5,16 @@ from sklearn.model_selection import train_test_split
 class Data :
 	def __init__(self, path: str) :
 		self.load(path)
-		self.transform()
+		self.prepare()
+
+		X ,Y = self.getTrainingData()
+		print(X)
+		print(Y)	
 
 
-	def load(self,path:str) :
-		self.df = pd.read_csv(path,parse_dates=["hire_date"])
-
-
-	def transform(self) :
+	def prepare(self) :
 		today = pd.to_datetime("today")
-		self.df["employed"] = (today - self.df["hire_date"]).dt.days / 365
+		self.df["tenure"] = (today - self.df["hire_date"]).dt.days / 365
 
 		for i,row in self.df.iterrows() :
 			if (row["job_id"] == "CEO") : self.df.at[i,"job"] = 1
@@ -26,10 +26,39 @@ class Data :
 			elif (row["job_id"] == "SNRTSC") :	self.df.at[i,"job"] = 7
 			elif (row["job_id"] == "SNRCONS") : self.df.at[i,"job"] = 8
 
+			self.split()
+
+
+	def load(self,path:str) :
+		self.df = pd.read_csv(path,parse_dates=["hire_date"])
+
+
 	def split(self) :
-		self.X = self.df.drop("risc",axis=1)
-		self.y = self.df["status"]
-		self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X,self.y,test_size=0.2,random_state=42)
+		shuffled = self.df.sample(frac=1, random_state=42).reset_index(drop=True)
+
+		tr = int(len(shuffled) * 0.8)
+		tst = int((len(shuffled) - tr)/2)
+
+		self.train = shuffled[:tr]
+
+		self.test = shuffled[tr:tr+tst]
+		self.validate = shuffled[tr+tst:]
+
+		self.target = ["attrition"]
+		self.features = ["job","salary","tenure"]
+
+
+	def getTrainingData(self) :
+		return self.train[self.features], self.train[self.target]
+
+
+	def getTestData(self) :
+		return self.test[self.features], self.test[self.target]
+
+
+	def getValidationData(self) :
+		return self.validate[self.features], self.validate[self.target]
+
+
 
 data = Data("/Users/alhof/Repository/GenAI/bronze/attrition.csv")
-print(data.df)
